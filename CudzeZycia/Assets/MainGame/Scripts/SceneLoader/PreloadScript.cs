6686f67e820066c94c46771be56e02f23700a54e
GorlikItsMe <33355238+GorlikItsMe@users.noreply.github.com>
2022-07-07 22:06:37 +0200

    Fix PreloadScript

--- CudzeZycia/Assets/MainGame/Scripts/SceneLoader/PreloadScript.cs
+++ CudzeZycia/Assets/MainGame/Scripts/SceneLoader/PreloadScript.cs
@@ -4,15 +4,25 @@ using UnityEngine.SceneManagement;
 public class PreloadScript : MonoBehaviour
 {
 
-#if UNITY_EDITOR
+
     private void Awake()
     {
+        bool isEditor = false;
+#if UNITY_EDITOR
+        isEditor = true;
         DontDestroyOnLoad(gameObject);
         if (LoadingSceneIntegration.otherScene > 0)
         {
             // Debug.Log("Returning again to the scene: " + LoadingSceneIntegration.otherScene);
             SceneManager.LoadScene(LoadingSceneIntegration.otherScene);
         }
-    }
 #endif
+
+        if (!isEditor)
+        {
+            // to nie jest edytor tylko gotowy build wiec w³acz menu
+            SceneManager.LoadScene(1);
+        }
+    }
+
 }
